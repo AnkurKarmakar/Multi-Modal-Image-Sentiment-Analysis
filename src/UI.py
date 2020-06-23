2a7db23c7aeddee9afaee02c32592781833507ff
Ankur Karmakar <ankurkarmakar25126871@gmail.com>
2020-06-23 19:09:13 +0530

    Add files via upload

--- src/UI.py
+++ src/UI.py
@@ -19,12 +19,15 @@ def browse():
 def run():
     inputValue=textBox.get("1.0","end-1c")
     os.system('python image_emotion_gender_demo.py '+inputValue)
-textBox=Text(window, height=2, width=30)
-textBox.pack()
+textBox=Text(window, height=2, width=60)
+textBox.place(x=30,y=20)
+#textBox.pack()
 btn2 = Button(window, text="Browse", bg="black", fg="white",command=browse)
 btn = Button(window, text="Analyze", bg="black", fg="white",command=run)
-btn2.pack()
-btn.pack()
+btn2.place(x=250,y=70)
+btn.place(x=250,y=130)
+#btn2.pack()
+#btn.pack()
 
 window.mainloop()
 
